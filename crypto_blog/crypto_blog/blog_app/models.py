bc111867c9cc6c09e3cec3f69b76cc4d5da3f941
Ian Maloney <85586358+ianmaloney14@users.noreply.github.com>
2022-11-28 19:06:21 -0800

    add logo link

--- crypto_blog/crypto_blog/blog_app/models.py
+++ crypto_blog/crypto_blog/blog_app/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime, date
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
+    title_tag = models.CharField(max_length=255, default="title")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
+    post_date = models.DateField(auto_now_add=True)
+    description = models.CharField(max_length=255, default="In this article...")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    # thumbnail = models.ImageField()
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
--- crypto_blog/crypto_blog/blog_app/views.py
+++ crypto_blog/crypto_blog/blog_app/views.py
@@ -5,6 +5,12 @@ from .models import Post
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+    # ordering = ['-id']
+    ordering = ['-created_at']
+
+# def home(request):
+#     post_list = Post.objects.all()
+#     return render(request, 'home.html', {'post_list': post_list})
 
 class ArticleDetailView(DetailView):
     model = Post
