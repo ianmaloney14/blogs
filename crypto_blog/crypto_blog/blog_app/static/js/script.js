21e884228bd2a1e77d6b0147b8a78107325c2791
Ian Maloney <85586358+ianmaloney14@users.noreply.github.com>
2022-12-03 14:25:37 -0800

    responding to smaller devices

--- crypto_blog/crypto_blog/blog_app/static/js/script.js
+++ crypto_blog/crypto_blog/blog_app/static/js/script.js
@@ -22,29 +22,6 @@ document.addEventListener('click', e => {
     })
 })
 
+// COLOR CODE CATEGORIES
 
-
-// //STICKY NAVIGATION
-// const sectionHeroEl = document.querySelector(".section-hero");
-
-// const obs = new IntersectionObserver(
-//     function (entries) {
-//         const ent = entries[0];
-//         console.log(ent);
-
-//         if (ent.isIntersecting === false) {
-//             document.body.classList.add("sticky");
-//         }
-
-//         if (ent.isIntersecting) {
-//             document.body.classList.remove("sticky")
-//         }
-//     },
-//     {
-//         root: null,
-//         threshold: 0,
-//         rootMargin: "-80px",
-//     }
-// );
-
-// obs.observe(sectionHeroEl);
+// MOBILE NAV
