f881b8b9886b0e1759199fc95f767e1e602d2701
Ian Maloney <85586358+ianmaloney14@users.noreply.github.com>
2022-12-08 22:19:09 -0800

    navbar rework

--- crypto_blog/crypto_blog/blog_app/static/js/script.js
+++ crypto_blog/crypto_blog/blog_app/static/js/script.js
@@ -6,30 +6,30 @@ yearEl.textContent = currentYear;
 
 // NAV DROPDOWN
 
-document.addEventListener('click', e => {
-    const isDropdownButton = e.target.matches('[data-dropdown-button]')
-    if (!isDropdownButton && e.target.closest('[data-dropdown]') != null) return
+// document.addEventListener('click', e => {
+//     const isDropdownButton = e.target.matches('[data-dropdown-button]')
+//     if (!isDropdownButton && e.target.closest('[data-dropdown]') != null) return
 
-    let currentDropdown
-    if (isDropdownButton) {
-        currentDropdown = e.target.closest('[data-dropdown]')
-        currentDropdown.classList.toggle('active')
-    }
+//     let currentDropdown
+//     if (isDropdownButton) {
+//         currentDropdown = e.target.closest('[data-dropdown]')
+//         currentDropdown.classList.toggle('active')
+//     }
 
-    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
-        if (dropdown === currentDropdown) return 
-        dropdown.classList.remove('active')
-    })
-})
+//     document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+//         if (dropdown === currentDropdown) return 
+//         dropdown.classList.remove('active')
+//     })
+// })
 
 // MOBILE NAV
 
-const btnNavEl = document.querySelector('.btn-mobile-nav')
-const headerEl = document.querySelector('.header')
+// const btnNavEl = document.querySelector('.btn-mobile-nav')
+// const headerEl = document.querySelector('.header')
 
-btnNavEl.addEventListener('click', function() {
-    headerEl.classList.toggle('nav-open')
-})
+// btnNavEl.addEventListener('click', function() {
+//     headerEl.classList.toggle('nav-open')
+// })
 
 // COLOR CODE CATEGORIES
 
