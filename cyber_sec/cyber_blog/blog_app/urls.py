48c59ea00ef41f9d11e2aee1254926799ccdbf6d
Ian Maloney <85586358+ianmaloney14@users.noreply.github.com>
2022-12-29 21:50:55 -0800

    setup folder structure

--- cyber_sec/cyber_blog/blog_app/urls.py
+++ cyber_sec/cyber_blog/blog_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import HomeView, ArticleDetailView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('article/<int:pk>', ArticleDetailView.as_view(), name="article-details"),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
