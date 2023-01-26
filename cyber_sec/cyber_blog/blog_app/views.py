0423bb000e1bd68c7b4eeada351484a2ca788f25
Ian Maloney <85586358+ianmaloney14@users.noreply.github.com>
2023-01-25 21:25:00 -0800

    pagination styling

--- cyber_sec/cyber_blog/blog_app/views.py
+++ cyber_sec/cyber_blog/blog_app/views.py
@@ -6,7 +6,7 @@ from django.core.paginator import Paginator
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
-    paginate_by = 1
+    paginate_by = 2
 
 class ArticleDetailView(DetailView):
     model = Post
