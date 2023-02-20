35212fd2746e054d2b03b8d3ee7a62d6b2c1b2cc
Ian Maloney <85586358+ianmaloney14@users.noreply.github.com>
2023-02-20 13:16:26 -0800

    social links | privacy policy, terms & condition

--- cyber_sec/cyber_blog/blog_app/urls.py
+++ cyber_sec/cyber_blog/blog_app/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('article/<int:pk>', ArticleDetailView.as_view(), name="article-details"),
     path('about/', TemplateView.as_view(template_name="about.html"), name="about"),
+    path('terms/', TemplateView.as_view(template_name="terms.html"), name="terms"),
+    path('privacy/', TemplateView.as_view(template_name="privacy.html"), name="privacy"),
     path('add-post/', AddPostView.as_view(), name="add-post"),
     path('article/edit/<int:pk>', UpdatePostView.as_view(), name="update-post"),
     path('article/<int:pk>/delete', DeletePostView.as_view(), name="delete-post"),
